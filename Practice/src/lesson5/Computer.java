14a3584a372a3c4e99175b965d80b7cf62130ef2
SamsonWeldegebriel <sweldegebriel@mum.edu>
2017-09-12 14:14:24 -0500

    Overriding object methods
    Implemented equals(), hashCode() and toString()

--- Practice/src/lesson5/Computer.java
+++ Practice/src/lesson5/Computer.java
@@ -11,6 +11,11 @@ public class Computer {
 	public Computer(String manufacturer,String processor, int ramSize, 
 			int diskSize, double processorSpeed) {
 		// Make a Constructor to initialize the instance fields
+		this.manufacturer = manufacturer;
+		this.processor = processor;
+		this.ramSize = ramSize;
+		this.diskSize = diskSize;
+		this.processorSpeed = processorSpeed;
 	}
 
 	public int getRamSize() {
@@ -27,77 +32,59 @@ public class Computer {
 	
 	double computePower()
 	{// return ramSize multiplied by processorSpeed
-		return 0.0;
+		return getDiskSize() * getProcessorSpeed();
 	}
 
 	//Override the toString() method to display the current status
 	@Override
-	public String toString() {
-		return "Computer [manufacturer=" + manufacturer + ", processor="
-				+ processor + ", ramSize=" + ramSize + ", diskSize=" + diskSize
-				+ ", processorSpeed=" + processorSpeed + "]";
+	
+	public String toString(){
+		String str;
+		str = "[" + this.manufacturer + ", " + this.processor + ", " + this.ramSize+ ", " + this.diskSize+ ", " + this.processorSpeed +"]";
+		return str;
 	}
+	
+	// manufacturer, processor,  ramSize, diskSize, processorSpeed
 	
 	//Override the equals() and hashCode() method for the Computer class.
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
+	public boolean equals(Object ob)
+	{
+		if(ob == null) return false;
+		if(ob instanceof Computer)
+		{
+			Computer c = (Computer) ob;
+			if(this.manufacturer.equals(c.manufacturer) && this.processor.equals(c.processor) &&
+				this.ramSize == c.ramSize && this.diskSize == c.diskSize && 
+				this.processorSpeed == c.processorSpeed	)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Computer other = (Computer) obj;
-		if (diskSize != other.diskSize)
-			return false;
-		if (manufacturer == null) {
-			if (other.manufacturer != null)
-				return false;
-		} else if (!manufacturer.equals(other.manufacturer))
-			return false;
-		if (processor == null) {
-			if (other.processor != null)
-				return false;
-		} else if (!processor.equals(other.processor))
-			return false;
-		if (Double.doubleToLongBits(processorSpeed) != Double
-				.doubleToLongBits(other.processorSpeed))
-			return false;
-		if (ramSize != other.ramSize)
-			return false;
-		return true;
-	}	
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + diskSize;
-		result = prime * result
-				+ ((manufacturer == null) ? 0 : manufacturer.hashCode());
-		result = prime * result
-				+ ((processor == null) ? 0 : processor.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(processorSpeed);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ramSize;
-		return result;
+		}		
+		return false;
 	}
 	
+	@Override
+	public int hashCode()
+	{
+		
+		int hashcode = 7 * manufacturer.hashCode() * processor.hashCode() * Integer.valueOf(ramSize)  * Integer.valueOf(ramSize).hashCode()  * Integer.valueOf(diskSize).hashCode() * Double.valueOf(processorSpeed).hashCode() ;
+		return hashcode;
+	}
 	
 
 	public static void main(String[] args) {
 
 		Computer c1 = new Computer("Dell","intel i-5", 8, 500, 2.3);
 		Computer c2 = new Computer("Dell","intel i-5", 8, 500, 2.3);
-		Computer c3 = new Computer("Acer","intel i-7", 4, 1000, 2.0);
+		Computer c3 = new Computer("Acer","intel i-7", 4, 100, 2.3);
 		
 		System.out.println(c1.toString());
+		
 		System.out.println(c1.equals(c2));
 		System.out.println(c2.equals(c3));
 		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c3.hashCode());
 
 	}
 

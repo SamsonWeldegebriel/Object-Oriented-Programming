f36e64fa253d3dc29ca4a02d82ad2408b16d92b9
SamsonWeldegebriel <sweldegebriel@mum.edu>
2017-09-11 19:05:22 -0500

    Testing Unicode and Eclipse
    Testing Unicode and Eclipse

---
+++ Practice/src/lesson1/Welcome.java
@@ -0,0 +1,14 @@
+package lesson1;
+/**
+ * 
+ * @author 986015
+ *
+ *//**Welcome doc*/
+
+public class Welcome {
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Welcome");
+	}
+}

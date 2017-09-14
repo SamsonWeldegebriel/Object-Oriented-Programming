d4337f389a7569bc4ae7b5489f98147c9bb44b8c
SamsonWeldegebriel <sweldegebriel@mum.edu>
2017-09-14 15:48:06 -0500

    Swing Rainbow Solution
    Swing Rainbow Solution

--- Practice/src/lesson6/Rainbow.java
+++ Practice/src/lesson6/Rainbow.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import javax.swing.border.Border;
+
 
 public class Rainbow {
 	
@@ -24,7 +24,7 @@ public class Rainbow {
 		mainFrame.setTitle("Rainbow Color Frame");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton red = new JButton("Red");
+		JButton red = new JButton();
 		red.setBounds(0, 0, 100, 100);
 		red.setBackground(Color.red);
 		red.setBorderPainted(false);
@@ -40,7 +40,7 @@ public class Rainbow {
 	
 		
 		
-		JButton orange = new JButton("Orage");
+		JButton orange = new JButton();
 		orange.setBounds(100, 0, 100, 100);
 		orange.setBackground(Color.orange);
 		orange.setBorderPainted(false);
@@ -55,90 +55,76 @@ public class Rainbow {
 		
 		
 		
-		JButton green = new JButton("green");
+		JButton green = new JButton();
 		green.setBounds(200, 0, 100, 100);
 		green.setBackground(Color.green);
 		green.setBorderPainted(false);
 		mainFrame.add(green);
 		
-		red.addActionListener(new ActionListener(){
+		green.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(mainFrame,
-				"Red signifies passion, vitality, enthusiasm and security.");
+				"Green denotes fertility, growth, balance, health and wealth");
 			}});
 		
 		
-		JButton blue = new JButton("blue");
+		JButton blue = new JButton();
 		blue.setBounds(300, 0, 100, 100);
 		blue.setBackground(Color.blue);
-		red.setBorderPainted(false);
+		blue.setBorderPainted(false);
 		mainFrame.add(blue);
 		
-		red.addActionListener(new ActionListener(){
+		blue.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(mainFrame,
-				"Red signifies passion, vitality, enthusiasm and security.");
+				"Blue: The sky and the wide oceans are in this colour and hence it has been associated with Spirituality and Divinity");
 			}});
 		
 		
-		JButton yellow = new JButton("yellow");
+		JButton yellow = new JButton();
 		yellow.setBounds(400, 0, 100, 100);
 		yellow.setBackground(Color.yellow);
 		yellow.setBorderPainted(false);
 		mainFrame.add(yellow);
 		
-		red.addActionListener(new ActionListener(){
+		yellow.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(mainFrame,
-				"Red signifies passion, vitality, enthusiasm and security.");
+				"Yellow represents clarity of thought, wisdom, orderliness and energy");
 			}});
 		
-		JButton indigo = new JButton("Indigo");
+		JButton indigo = new JButton();
 		indigo.setBounds(500, 0, 100, 100);
 		indigo.setBackground(new Color(75,0,130));
 		indigo.setBorderPainted(false);
 		mainFrame.add(indigo);
 		
-		red.addActionListener(new ActionListener(){
+		indigo.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(mainFrame,
-				"Red signifies passion, vitality, enthusiasm and security.");
+				"Indigo is sedating.");
 			}});
 		
-		JButton violet = new JButton("violet");
+		JButton violet = new JButton();
 		violet.setBounds(600, 0, 100, 100);
 		violet.setBackground(new Color(148, 0, 211));
 		violet.setBorderPainted(false);
 		mainFrame.add(violet);
 		
-		red.addActionListener(new ActionListener(){
+		violet.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(mainFrame,
-				"Red signifies passion, vitality, enthusiasm and security.");
+				"Violet is considered the highest element of spirituality.");
 			}});
 	}
 	
 	public static void main(String[] args) {
 		new Rainbow();
 	}
-	
-	/*
-	 * 
-	 * 
-	 *  It is the light with the longest wavelength.
-Orange– This light or color is a combination of yellow and red. 
-Yellow– This is the color of the sunshine itself. It represents clarity of thought, wisdom, orderliness and energy.
-Green– This is the middle color of the rainbow and denotes fertility, growth, balance, health and wealth.
-Blue– This the fifth color of the rainbow which makes us think of the Unknown. The sky and the wide oceans are in this color and hence it has been associated with Spirituality and Divinity.
-Indigo– It is believed that where Blue is calming, Indigo is sedating. Indigo is mystical as it bridges the Gap between Finite and Infinite. Indigo colored gem stones are often used for spiritual attainment, psychic abilities, self awareness and enhancement of Intuition.
-Violet– The last color of the rainbow is a mix of red and blue. It is considered the highest element of spirituality. It can ignite one’s imagination and be an inspiration to artists. Dark tones of violet are associated with sorrow. Deeper shades of violet or purple denote high spiritual mastery.
-	 * 
-	 * 
-	 * */
 
 }

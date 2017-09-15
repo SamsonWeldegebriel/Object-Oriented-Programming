5cdc105df7c07a3fd446dea5aac6f1daa7b359aa
SamsonWeldegebriel <sweldegebriel@mum.edu>
2017-09-15 16:27:23 -0500

    Inner Classes and GUI

---
+++ Practice/src/innerclasses/MyTable.java
@@ -0,0 +1,68 @@
+package innerclasses;
+
+public class MyTable {
+	private int i;
+	private Entry[] entries = new Entry[26];
+		
+	// returns the String that is matched with char c in the table
+	public String get(char c) {
+		String str = "";
+		for(Entry e: entries)
+		{
+			if(e != null)
+			if(e.index == c)
+			{
+				
+				str = e.value;
+			}
+		}
+		
+		
+		return str;
+	}
+
+	// adds to the table a pair (c, s) so that s can be looked up using c
+	public void add(char c, String s) {
+		this.i = c % 97;
+		entries[i] = new Entry(c,s);
+	}
+
+	// returns a String consisting of nicely formatted display
+	// of the contents of the table
+	public String toString() {
+		String ret = "";
+		for(Entry e:entries)
+		{
+			if(e != null)
+			ret = ret + "\n" + e.toString();
+		}
+		return ret;
+	}
+
+	private class Entry {
+		private char index;
+		private String value;
+		
+		Entry(char ch, String str) {
+			
+			this.index = ch;
+			this.value = str;
+		}
+
+		// returns a String of the form "ch->str"
+		public String toString() {
+			String str = index + "->" + value;
+			return str;
+		}
+	}
+	
+	public static void main(String[] args) {
+		MyTable t = new MyTable();
+		t.add('a', "Andrew");
+		t.add('b',"Billy");
+		t.add('c',"Charlie");
+		String s = t.get('b');
+		System.out.println(s);
+		System.out.println(t);
+	}
+}
